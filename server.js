01d99777ab226db08776a817f0021d1b4e3d714a
aditya-ahlawat-1309 <adityaroyal10@gmail.com>
2022-06-08 15:19:34 +0530

    project 5

---
+++ server.js
@@ -0,0 +1,143 @@
+const express = require("express");
+const cors = require("cors");
+const mongoose = require("mongoose");
+const dotenv = require("dotenv");
+const app = express();
+
+
+// to use dotenv files
+dotenv.config();
+
+///////////////////////////////////////////////////////////////////////////////
+
+//Cors Configuration - Start
+// always use this to remove CORS Error
+
+app.use(function (req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Methods", "GET,HEAD,OPTIONS,POST,PUT");
+  res.header(
+    "Access-Control-Allow-Headers",
+    "Origin, X-Requested-With, Content-Type, Accept, Authorization"
+  );
+  next();
+});
+//Cors Configuration - End
+
+//////////////////////////////////////////////////////////////////////////////////
+
+
+
+const ObjectId = require("mongoose").Types.ObjectId;
+
+mongoose.connect(
+  "mongodb+srv://aditya:8901459471@cluster0.sizzc.mongodb.net/?retryWrites=true&w=majority",
+  {
+    useUnifiedTopology: true,
+    useNewUrlParser: true,
+  }
+);
+
+const userSchema = new mongoose.Schema({
+  username: String,
+  password: String,
+});
+const User = mongoose.model("User", userSchema);
+
+const todosSchema = new mongoose.Schema({
+  userId: mongoose.Schema.ObjectId,
+  todos: [
+    {
+      checked: Boolean,
+      text: String,
+      id: String,
+    },
+  ],
+});
+const Todos = mongoose.model("Todos", todosSchema);
+
+app.use(cors());
+app.use(express.json());
+
+app.post("/register", async (req, res) => {
+  const { username, password } = req.body;
+  const user = await User.findOne({ username }).exec();
+  if (user) {
+    res.status(500);
+    res.json({
+      message: "user already exists",
+    });
+    return;
+  }
+  await User.create({ username, password });
+  res.json({
+    message: "success",
+  });
+});
+
+app.post("/login", async (req, res) => {
+  const { username, password } = req.body;
+  const user = await User.findOne({ username }).exec();
+  if (!user || user.password !== password) {
+    res.status(403);
+    res.json({
+      message: "invalid login",
+    });
+    return;
+  }
+  res.json({
+    message: "success",
+  });
+});
+
+app.post("/todos", async (req, res) => {
+  const { authorization } = req.headers;
+  const [, token] = authorization.split(" ");
+  const [username, password] = token.split(":");
+  const todosItems = req.body;
+  const user = await User.findOne({ username }).exec();
+  if (!user || user.password !== password) {
+    res.status(403);
+    res.json({
+      message: "invalid access",
+    });
+    return;
+  }
+  const todos = await Todos.findOne({ userId: user._id }).exec();
+  if (!todos) {
+    await Todos.create({
+      userId: user._id,
+      todos: todosItems,
+    });
+  } else {
+    todos.todos = todosItems;
+    await todos.save();
+  }
+  res.json(todosItems);
+});
+
+app.get("/todos", async (req, res) => {
+  const { authorization } = req.headers;
+  const [, token] = authorization.split(" ");
+  const [username, password] = token.split(":");
+  const user = await User.findOne({ username }).exec();
+  if (!user || user.password !== password) {
+    res.status(403);
+    res.json({
+      message: "invalid access",
+    });
+    return;
+  }
+  const { todos } = await Todos.findOne({ userId: user._id }).exec();
+  res.json(todos);
+});
+
+const PORT = process.env.PORT || 8000;
+
+const db = mongoose.connection;
+db.on("error", console.error.bind(console, "connection error:"));
+db.once("open", function () {
+  app.listen(PORT, () => {
+    console.log(`Example app listening at http://localhost:${PORT}`);
+  });
+});
